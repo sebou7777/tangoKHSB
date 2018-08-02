06f7ae6d41faf0db91cb36fd937fe29842c35c9a
Sébastien Bourgeois <sebastien.bourgeois@groupe-easycom.com>
2018-08-02 18:06:19 +0200

    charte rapide de la colonne droite

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div class="box menu tostick">
-    <p class="menu-label">CATEGORIES</p>
+    <h4>CATEGORIES</h4>
     <ul class="menu-list">
         <?php foreach(tango_get_categories() as $category): ?>
             <li><a <?php echo (($category->is_selected) ? 'class="is-active"' : '') ?> href="<?php echo get_category_link($category) ?>"><?php echo $category->name ?></a></li>
@@ -7,7 +7,7 @@
     </ul>
 </div>
 <div class="box menu">
-    <p class="menu-label">ARTICLES RECENTS</p>
+    <h4>ARTICLES RECENTS</h4>
     <ul class="menu-list">
         <?php foreach(tango_get_recents() as $post): ?>
             <li><a href="<?php echo $post->post_url ?>"><?php echo $post->post_title ?></a></li>
